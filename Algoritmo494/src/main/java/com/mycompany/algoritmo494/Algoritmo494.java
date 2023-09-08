dda45eb4895cf608ed6e702f0ad00a4048d0071f
Dgandalecio <127789911+Dgandalecio@users.noreply.github.com>
2023-09-08 19:18:47 -0300

    Add files via upload

---
+++ Algoritmo494/src/main/java/com/mycompany/algoritmo494/Algoritmo494.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.algoritmo494;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author diogo
+ */
+public class Algoritmo494 {
+
+    public static void main(String[] args) {
+        int L,c,t,tl;
+        Valores1 pr = new Valores1();
+        
+        for(L=0;L<3;L++){
+            
+            pr.nome[L]=JOptionPane.showInputDialog("Digite o nome");
+            pr.ender[L]=JOptionPane.showInputDialog("Digite o endereco");
+            pr.prof[L]=JOptionPane.showInputDialog("Digite a profissao");
+        }
+          for(L=0;L<2;L++){
+              
+              for(c=(L+1);c<3;c++){
+                  if (pr.nome[L].compareTo(pr.nome[c]) > 0){
+                      
+                      pr.troca(pr.nome[L],pr.nome[c]);
+                      pr.troca(pr.ender[L],pr.ender[c]);
+                      pr.troca(pr.prof[L], pr.ender[c]);
+                      
+                  }
+              }
+          }
+           for(L=0;L<3;L++){
+               System.out.println(pr.nome[L]+"\t"+pr.ender[L]+"\t"+pr.prof[L]);
+           }
+    }
+}
---
+++ Algoritmo494/src/main/java/com/mycompany/algoritmo494/Valores1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.algoritmo494;
+
+/**
+ *
+ * @author diogo
+ */
+public class Valores1 {
+
+String nome[]= new String[3];
+String prof[]= new String[3];
+String ender[]= new String[3];
+
+
+public static String troca(String n1, String n2){
+    
+    String aux;
+    aux=n1;
+    n1=n2;
+    n2=aux;
+    return aux;
+}
+
+}
+
